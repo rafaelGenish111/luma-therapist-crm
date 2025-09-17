533c80d4d76c394360b61839fd7abb717701dcf8
rafaelGenish111 <rafaelgenish111@gmail.com>
2025-09-17 09:35:40 +0300

    Fix auth middleware import in enhancedPayments
    - Change from require('../middleware/auth') to require('../middleware/auth').auth
    - Fix Express route callback function error
    - All routes now properly use auth function instead of auth object

--- server/src/routes/enhancedPayments.js
+++ server/src/routes/enhancedPayments.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../middleware/auth');
+const { auth } = require('../middleware/auth');
 const authorize = require('../middleware/authorize');
 const enhancedBillingService = require('../services/enhancedBillingService');
 
